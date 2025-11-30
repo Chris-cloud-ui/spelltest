b92575d93fa59bc5d76135a5ddfc94f7fd4ef9ae
Chris-cloud-ui <84877364+Chris-cloud-ui@users.noreply.github.com>
2025-11-30 19:28:37 +0000

    Update weeb.py

--- weeb.py
+++ weeb.py
@@ -183,17 +183,17 @@ else:
                     
                 st.info("Current score: " + str(st.session_state.score) + "/" + str(st.session_state.index + 1))
                 st.session_state.submitted = True
-                #if st.button("Next Word"):
-                #    
-                #    st.session_state.index += 1
-                #    st.session_state.current_mode = None
-                #    st.session_state.submitted = False
-                #    st.session_state.user_word_value = ""
-                # 
-                #    if st.session_state.index >= len(st.session_state.words):
-                #        st.session_state.done = True
+                if st.button("Next Word"):
+                    
+                    st.session_state.index += 1
+                    st.session_state.current_mode = None
+                    st.session_state.submitted = False
+                    st.session_state.user_word_value = ""
+                 
+                    if st.session_state.index >= len(st.session_state.words):
+                        st.session_state.done = True
             
-                #    st.rerun()
+                    st.rerun()
         else:
             st.session_state.index += 1
             st.session_state.current_mode = None
@@ -320,6 +320,7 @@ else:
             🔤 Misspellings: {entry['misspellings']} 
             <br><br>
         """, unsafe_allow_html=True)
+
 
 
 
